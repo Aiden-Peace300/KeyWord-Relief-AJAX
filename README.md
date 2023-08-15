dc9a9002c0baca9c5959cbf567474d029759c51b
Aiden Peace <aidenpeace300@gmail.com>
2023-08-15 21:55:30 +0000

    Completed the forth requierment from the user-can-view-five-words-matching-their-definition task list 4) Create a function named getMsgData that will need one parameter which is the user's input 'def' and will return the user's five definition

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ajax-project
 
 A dynamic HTML, CSS, and JavaScript solo project.
+
+- Promise MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
--- js/main.js
+++ js/main.js
@@ -3,33 +3,84 @@ const $form = document.getElementById('input-form');
 const $submitButton = document.querySelector('.submit-button');
 const $defInput = document.getElementById('def');
 
-// Add 'click' event listener to the submit button
-$submitButton.addEventListener('click', function (event) {
-  event.preventDefault(); // Prevent page from refreshing
+// Function to make a request to the OpenAI API using XMLHttpRequest
+function getMsgData(name) {
 
-  // Get the input value from the textarea
+  // Promise method produces a value after an asynchronous (aka, async) operation completes successfully
+  return new Promise((resolve, reject) => {
+
+    // Create an XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+    const apiKey = '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';
+    const url = 'https://api.openai.com/v1/chat/completions';
+
+    // Configure the request
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.responseType = 'json';
+
+    // Event listener for when the response is loaded
+    xhr.addEventListener('load', function () {
+      const chatGptMsg = xhr.response.choices[0].message.content;
+      resolve(chatGptMsg); // Resolve the Promise with the response
+    });
+
+    // Data to be sent in the request body
+    const requestData = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: name
+        },
+        {
+          role: 'system',
+          content: 'You are a helpful assistant.'
+        }
+      ]
+    };
+
+    // Send the request with the JSON data
+    xhr.send(JSON.stringify(requestData));
+  });
+}
+
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault();
+
   const definition = $defInput.value;
 
   if (definition.length === 0) {
-    alert('Please enter a definition.'); // Add validation for empty input
+    alert('Please enter a definition.');
     return;
   }
 
-  // Create a new entry object
-  const newEntry = {
-    entryId: data.nextEntryId,
-    definition
-  };
+  // Call the API function and handle the response
+  getMsgData('GIVE ME A LIST OF 5 WORDS (NUMBERED) AND A SIMPLE DEF OF EACH OF THEM THAT MAY MATCH THIS DEF USE COLONS (NO BETWEEN NUMBER AND WORD) IN BETWEEN WORD AND DEF :' + definition)
+    .then(response => {
 
-  // Increment the nextEntryId in the data model
-  data.nextEntryId++;
+      // Create a new entry object
+      const newEntry = {
+        entryId: data.nextEntryId,
+        definition,
+        response
+      };
 
-  // Add the new entry to the beginning of the entries array
-  data.entries.unshift(newEntry);
+      // Increment the entry ID and update the entries array
+      data.nextEntryId++;
+      data.entries.unshift(newEntry);
 
-  // Clear the input
-  $defInput.value = '';
+      // Clear the input and reset the form
+      $defInput.value = '';
+      $form.reset();
 
-  // Reset the form
-  $form.reset();
-});
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+// Add a 'click' event listener to the submit button
+$submitButton.addEventListener('click', handleSubmit);
