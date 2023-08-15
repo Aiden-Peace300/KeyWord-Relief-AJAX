17169495e598add1f39f7be649dcc95e4a9cb758
Aiden Peace <aidenpeace300@gmail.com>
2023-08-15 03:10:06 +0000

    Completed the sixth requierment from the user-can-create-an-entry task list 6) In data.js, add a 'beforeunload' event listener to the window object that serializes the data model as JSON and stores the JSON in localStorage.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,19 @@
 /* exported data */
+// When the user is about to leave or reload the webpage,
+// the handleStorage function will be executed.
+window.addEventListener('beforeunload', handleStorage);
+
+let data = {
+  view: 'entry-form',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+function handleStorage(event) {
+  // stringify() => static method converts a JavaScript value to a JSON string
+  const dataJSON = JSON.stringify(data);
+
+  // setItem() => store data in the browser's local storage
+  localStorage.setItem('Javascript-local-storage', dataJSON);
+}
